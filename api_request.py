3e5747efc922daf95147a8f79c3e1cd5a9e816e5
Euan Ramsay <euancramsay@aol.com>
2017-04-18 10:41:43 +0100

    Sorted line lenght issues and added __doc__ to Movie() class

--- api_request.py
+++ api_request.py
@@ -3,7 +3,10 @@ import requests
 
 def get_film_data(film_title):
 	# sends request to OMDb API to get film data
-	omdb_request = requests.get('http://www.omdbapi.com/?t=' + film_title)
+	omdb_request = (
+		requests.get('http://www.omdbapi.com/?t=' 
+		+ film_title)
+		)
 	
 	film_data = json.loads(omdb_request.text)
 
@@ -14,12 +17,20 @@ def get_film_data(film_title):
 	# sends request to search YouTube API based on film title
 	API_KEY = 'AIzaSyBUYgsqikPPzNpli9IrZbi1Ge9cfoaJ0IQ'
 
-	youtube_request = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&key=' + API_KEY + '&q='+ film_title + ' trailer')
+	youtube_request = (
+		requests.get(
+		'https://www.googleapis.com/youtube/v3/'
+		'search?part=snippet&maxResults=1&key=' 
+		+ API_KEY + '&q='+ film_title + ' trailer')
+		)
 
 	trailer_data = json.loads(youtube_request.text)
 
 	# creates full youtube url
-	youtube_url = 'https://www.youtube.com/watch?v=' + trailer_data['items'][0]['id']['videoId']
+	youtube_url = (
+		'https://www.youtube.com/watch?v=' 
+		+ trailer_data['items'][0]['id']['videoId']
+		)
 
 	# puts all data for a single film in an array
 	film_data = [title, storyline, poster, youtube_url]
--- entertainment_centre.py
+++ entertainment_centre.py
@@ -3,9 +3,12 @@ import media
 import film_data
 
 # array of the titles of all films to be displayed, this can be edited
-films = ["Buffalo 66", "GoodFellas", "Uncle Buck", "Annie Hall", "Indiana Jones and the Last Crusade", "Dazed and Confused"]
+films = (
+	["Buffalo 66", "GoodFellas", "Uncle Buck", "Annie Hall", 
+	"Indiana Jones and the Last Crusade", "Dazed and Confused"]
+	)
 
-# movies is an array of instances of Movie based on the titles in the array films
+# movies is an array of instances of Movie based on titles in the array films
 movies = film_data.create_movie_objects(films)
 
 fresh_tomatoes.open_movies_page(movies)
